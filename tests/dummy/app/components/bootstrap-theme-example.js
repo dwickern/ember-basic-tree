f26fa25a10e97351d2db7a6f0b3413bc1f8cf9e2
Derek Wickern <dwickern@gmail.com>
2017-11-10 12:26:53 -0800

    add bootstrap theme example

---
+++ tests/dummy/app/components/bootstrap-theme-example.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../templates/components/bootstrap-theme-example';
+
+export default Component.extend({
+  layout,
+  classNames: 'bootstrap-theme-example'
+});
