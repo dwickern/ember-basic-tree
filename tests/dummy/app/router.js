ae20e4f8da84cf3110b9817626c90a9809d79a05
Derek Wickern <dwickern@gmail.com>
2021-01-20 17:43:13 -0800

    upgrade ember-cli

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,24 +1,22 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from 'dummy/config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
-Router.map(function() {
-  this.route('navigation-example', function() {
-    this.route('child1', function() {
+Router.map(function () {
+  this.route('navigation-example', function () {
+    this.route('child1', function () {
       this.route('descendant1');
       this.route('descendant2');
       this.route('descendant3');
     });
-    this.route('child2', function() {
+    this.route('child2', function () {
       this.route('descendant1');
       this.route('descendant2');
       this.route('descendant3');
     });
   });
 });
-
-export default Router;
