31c5e89472bbf3a24917975b9ec6558211c0e367
Derek Wickern <dwickern@gmail.com>
2021-12-10 11:21:32 -0800

    upgrade to bootstrap 4

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,8 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     'ember-bootstrap': {
-      bootstrapVersion: 3,
-      importBootstrapFont: true,
+      bootstrapVersion: 4,
+      importBootstrapFont: false,
       importBootstrapCSS: false,
     },
     snippetSearchPaths: ['addon', 'tests/dummy/app'],
